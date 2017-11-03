bed5faa46d59abcb7aac16221dc8a89d1cb5d030
yezewei <yezewei@tourongjia.com>
2017-11-03 17:20:55 +0800

    sss

---
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/itcast/eurekaconsumerribbonhystrix/EurekaConsumerRibbonHystrixApplication.java
@@ -0,0 +1,12 @@
+package com.itcast.eurekaconsumerribbonhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConsumerRibbonHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaConsumerRibbonHystrixApplication.class, args);
+	}
+}
