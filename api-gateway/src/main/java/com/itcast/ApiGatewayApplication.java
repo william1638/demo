1e3cfdca69ff4601b7d86fdb4fd6e6796c7fb9b3
William <634898506@qq.com>
2017-11-05 20:42:20 +0800

    a

--- api-gateway/src/main/java/com/itcast/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/itcast/ApiGatewayApplication.java
@@ -1,12 +1,19 @@
 package com.itcast;
 
+import com.itcast.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
 public class ApiGatewayApplication {
+
+	@Bean
+	public AccessFilter accessFilter(){
+		return new AccessFilter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
